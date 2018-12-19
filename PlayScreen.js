7beb4420848534d6c64c2b78d9fd9f533f454f6d
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-12-19 09:46:16 +0100

    save input in array

--- PlayScreen.js
+++ PlayScreen.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
 
 
 export default class PlayScreen extends React.Component {
+
+
+  state = {
+    text: '',
+    words: [],
+  }
+
+  onChangeT = (value) => {
+    this.setState({
+      text: value
+    })
+  }
+
+  searchSubmit = () => {
+    alert('search')
+  }
+
+  onSave = () => {
+    if(this.state.text.length > 1){
+      this.setState(previousState => (
+        {
+        words: [...previousState.words, this.state.text]
+      }), () => console.log(this.state.words))
+//console.log('save', this.state.text)
+//console.log(this.state.words)
+    }
+  }
+
     render() {
       return (
         <View style={styles.container}>
           <Text style={styles.text}>Lista tjejnamn på A</Text>
-          <TextInput style={styles.input}/>
+          <Text style={styles.text}>{this.state.words}</Text>
+          <TextInput 
+          onChangeText={this.onChangeT}
+          style={styles.input}
+          onSubmitEditing={this.searchSubmit}
+          />
+           <TouchableHighlight                     
+           style={styles.button}
+            onPress={this.onSave}>
+                <Text>Enter</Text>
+            </TouchableHighlight>
         </View>
       );
     }
@@ -28,5 +66,18 @@ export default class PlayScreen extends React.Component {
             backgroundColor: '#e8e8e8',
             width: '60%',
             padding: 10
+        },
+
+        button:  {
+        height: 50,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        alignSelf: 'stretch',
+        margin: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 5,
+
         }
   })
