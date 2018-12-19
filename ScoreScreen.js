61781d6cfa48bf0d0d762cf117398ddc5552f552
amandaaab <32762144+amandaaab@users.noreply.github.com>
2018-12-19 15:10:33 +0100

    continue button

--- ScoreScreen.js
+++ ScoreScreen.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
 
 
 export default class ScoreScreen extends React.Component {
+
+    onPressContinue = () => {
+        this.props.navigation.navigate('Home')
+       
+    }
 
     render() {
 
@@ -12,6 +17,7 @@ export default class ScoreScreen extends React.Component {
       return (
         <View style={styles.container}>
          <Text style={styles.text}>{`Din poäng: ${score}`}</Text>
+         <TouchableHighlight onPress={this.onPressContinue} style={styles.continue}><Text>Gå vidare</Text></TouchableHighlight>
         </View>
       );
     }
@@ -26,6 +32,14 @@ export default class ScoreScreen extends React.Component {
     },
     text: {
         color: 'white'
+    },
+    continue: {
+        backgroundColor: '#65aa3d',
+        width: '100%',
+        height: 40,
+        borderRadius: 5,
+        alignItems: 'center',
+        padding: 10,
     }
 
 });
