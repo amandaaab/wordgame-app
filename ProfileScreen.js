92902122048b5eb8f6be68ef46d8cbf5aa0df594
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2019-01-10 11:39:23 +0100

    update profilescreen

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import * as firebase from 'firebase';
+import { Ionicons } from '@expo/vector-icons';
+
 
 export default class ProfileScreen extends React.Component {
   constructor(props){
@@ -30,13 +32,15 @@ export default class ProfileScreen extends React.Component {
 
     return (
       <View style={styles.container}>
+      <View style={styles.whiteContainer}>
+      <Ionicons name="md-contact" size={80}/>
         <Text style={styles.name}>{name}</Text>
         <Text style={styles.rating}>Rating</Text>
         <Text style={styles.rating}>⭐️ ⭐️ ⭐️ ⭐️ ⭐️ </Text>
         <Text style={styles.rounds}>Antal spelade omgångar: {this.props.screenProps.roundes[this.props.screenProps.roundes.length-1]}
-</Text>
-
-        <TouchableHighlight onPress={this.onPressLogout}>
+        </Text>
+      </View>
+        <TouchableHighlight style={styles.logoutButton} onPress={this.onPressLogout}>
             <Text>
               Logga ut
             </Text>
@@ -53,15 +57,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  whiteContainer: {
+    width: '80%',
+    height: '60%',
+    padding: 20,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 6,
+  },
   name: {
-    color: 'white',
+    color: 'grey',
     fontSize: 25,
+    margin: '4%',
   },
   rating: {
-    color: 'white',
+    color: 'grey',
+    margin: '2%',
+
   },
   rounds: {
-    color: 'white',
+    color: 'grey',
+    margin: '4%',
+
+  },
+  logoutButton: {
+    backgroundColor: '#f44141',
+        width: '80%',
+        height: 54,
+        borderRadius: 6,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        margin: 10
   }
 
 });
